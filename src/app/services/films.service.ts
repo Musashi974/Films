7b1cf3fb6371aae9254dca51289655b514e41ab4
Musashi974 <musashi974@gmail.com>
2023-04-03 08:56:23 -0400

    Initial commit

---
+++ src/app/services/films.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmsService {
+
+  constructor() { }
+}
